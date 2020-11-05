d2fdf35b43193a7152702da6845d8f4d5c399c69
qosqoruna1996 <jose.lovaton@unas.edu.pe>
2020-11-05 13:22:17 -0500

    commit inicial

---
+++ api/v1/routes.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+var jwt = require('jwt-simple');
+var payload = {}; // datos de usuario ;v
+var secret = '$2y$12$RM3QIuN.9P/KUb4fugZPru0cZ3Sn14r9ajUzWFPzyBGtOxPGjsiE2'; // esto deberíamos guardarlo en un .env pero me fale ferga
+var authMiddleware = require('../../middleware');
+
+const productos = [{
+        id: 1,
+        code: '1111',
+        name: 'mango',
+        precio: 4,
+        unidad: "kg"
+    },
+    {
+        id: 2,
+        code: '2222',
+        name: 'mandarina',
+        precio: 5,
+        unidad: "kg"
+    },
+    {
+        id: 3,
+        code: '3333',
+        name: 'limon',
+        precio: 3,
+        unidad: "kg"
+    },
+    {
+        id: 4,
+        code: '4444',
+        name: 'naranja',
+        precio: 3,
+        unidad: "kg"
+    }
+]
+
+
+router.get('/auth/token', function(req, res) {
+    var token = jwt.encode(payload, secret);
+    res.json(token);
+});
+
+
+router.get('/productos', [authMiddleware], function(req, res) {
+    res.status(200).json({
+        content: productos
+    });
+});
+
+
+module.exports = router;
